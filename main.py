5d2a32eef941256cad5ff87def25781906c81f98
DHWANIG20 <dhwanig162@gmail.com>
2025-10-29 11:26:41 +0530

    file added

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from count_words import count_words
+from find_longest import find_longest_word
 
+try:
+    with open("sample.txt", "r") as f:
+        content = f.read()
+    total = count_words(content)
+    longest, length = find_longest_word(content)
+    print("📘 WORD COUNTER RESULTS 📘")
+    print("----------------------------")
+    print(f"Total words: {total}")
+    print(f"Longest word: {longest} ({length} letters)")
+    print("----------------------------")
+except FileNotFoundError:
+    print("❌ sample.txt not found")
